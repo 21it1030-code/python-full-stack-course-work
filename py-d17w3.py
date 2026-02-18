889823b18f79a56e806b260719f1fc69ffd9d17f
21it1030-code <21it1030@gmail.com>
2026-02-18 22:35:43 +0530

    Add files via upload

---
+++ py-d17w3.py
@@ -0,0 +1,10 @@
+#   CASE CONVERSION
+x="prakash IS a FRONTEND DEVELOPER"
+print(x.lower())
+print(x.upper())
+print(x.swapcase())
+print(x.capitalize())
+print(x.islower())
+print(x.isupper())
+print(x.istitle())
+print(x.isprintable())
